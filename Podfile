6a05c93ef6855f44bbb806256525b99a62f2518d
zhangjiang <896884553@q.com>
2016-11-19 22:18:17 +0800

    添加了cocoapods类库

---
+++ Podfile
@@ -0,0 +1,7 @@
+target 'PhotoCatchTest' do
+   platform:ios,'8.0'
+   pod 'MBProgressHUD', '~> 1.0.0'
+
+
+
+end
---
+++ Pods/Target Support Files/Pods-PhotoCatchTest/Pods-PhotoCatchTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoCatchTest : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoCatchTest
+@end
